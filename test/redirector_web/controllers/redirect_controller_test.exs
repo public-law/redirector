3a091faea4bc0ccd0306e0ae74adb0c454aae972
Robb Shecter <robb@public.law>
2018-02-23 02:18:43 -0800

    chore: failing test for New York page

--- test/redirector_web/controllers/redirect_controller_test.exs
+++ test/redirector_web/controllers/redirect_controller_test.exs
@@ -11,13 +11,23 @@ defmodule RedirectorWeb.RedirectControllerTest do
            ]
   end
 
-  test "basic state redirect works", %{conn: conn} do
+  test "basic state redirect", %{conn: conn} do
     conn = get(conn, "/texas/statutes/tex._election_code")
 
     assert conn.status == 301
 
     assert get_resp_header(conn, "location") == [
              "https://texas.public.law/statutes/tex._election_code"
+           ]
+  end
+
+  test "leaf node New York redirect", %{conn: conn} do
+    conn = get(conn, "/new_york/laws/n.y._multiple_dwelling_law_section_2")
+
+    assert conn.status == 301
+
+    assert get_resp_header(conn, "location") == [
+             "https://newyork.public.law/laws/n.y._multiple_dwelling_law_section_2"
            ]
   end
 end
