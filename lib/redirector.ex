b6393be60b2dbb15d75e1444af1bbc5a36e9e122
Robb Shecter <robb@public.law>
2018-03-07 00:47:44 -0800

    feature: passing tests

--- lib/redirector.ex
+++ lib/redirector.ex
@@ -6,4 +6,8 @@ defmodule Redirector do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def preferred_visitor?(domain: domain) when is_bitstring(domain) do
+    String.ends_with?(domain, [".edu", ".gov"])
+  end
 end
