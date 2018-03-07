d5574e1f6e131d5f6ed58537bf9327415031b300
Robb Shecter <robb@public.law>
2018-03-07 01:40:18 -0800

    feature: use x-forwarded-for

--- lib/redirector_web/controllers/api_controller.ex
+++ lib/redirector_web/controllers/api_controller.ex
@@ -8,8 +8,17 @@ defmodule RedirectorWeb.ApiController do
   end
 
   def is_preferred_visitor(conn, _params) do
-    {a, b, c, d} = conn.remote_ip
-    ip = "#{a}.#{b}.#{c}.#{d}"
+    xff = Enum.find(conn.req_headers, fn {k, _v} -> k == "x-forwarded-for" end)
+
+    ip =
+      if xff != nil do
+        {"x-forwarded-for", ip_string} = xff
+        ip_string
+      else
+        {a, b, c, d} = conn.remote_ip
+        "#{a}.#{b}.#{c}.#{d}"
+      end
+
     {:ok, domain} = Host.reverse_lookup(ip: ip)
 
     answer =
