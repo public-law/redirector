5744109322756ad9cee5f4f9854ddb7912668b1c
Robb Shecter <robb@public.law>
2018-03-07 00:25:40 -0800

    chore: remote ip and domain

--- lib/redirector_web/controllers/api_controller.ex
+++ lib/redirector_web/controllers/api_controller.ex
@@ -2,8 +2,20 @@ defmodule RedirectorWeb.ApiController do
   use RedirectorWeb, :controller
 
   def is_preferred_visitor(conn, _params) do
+    {a, b, c, d} = conn.remote_ip
+    ip = "#{a}.#{b}.#{c}.#{d}"
+    {:ok, domain} = Host.reverse_lookup(ip: ip)
+
+    content = %{
+      "is_preferred_visitor" => "no",
+      "remote_ip" => ip,
+      "remote_domain" => domain
+    }
+
+    # TODO: preferred_origin?(conn.remote_ip)
+
     conn
     |> put_resp_content_type("text/json")
-    |> send_resp(200, "{\"is_preferred_visitor\": \"no\"}")
+    |> send_resp(200, Poison.encode!(content))
   end
 end
--- mix.exs
+++ mix.exs
@@ -6,9 +6,9 @@ defmodule Redirector.Mixfile do
       app: :redirector,
       version: "0.0.1",
       elixir: "~> 1.4",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
@@ -25,7 +25,7 @@ defmodule Redirector.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
@@ -35,7 +35,8 @@ defmodule Redirector.Mixfile do
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:host, "~> 0.1.0"}
     ]
   end
 end
