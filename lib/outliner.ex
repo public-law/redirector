853dd2e3bb6b645ed94affed540ec40c9b4d4b85
Robb Shecter <robb@public.law>
2019-05-01 20:41:57 -0700

    failing test for null case

--- lib/outliner.ex
+++ lib/outliner.ex
@@ -1,2 +1,9 @@
 defmodule Outliner do
+  @moduledoc """
+  Algorithms which re-format an outline into its inherent organization.
+  """
+
+  # Convert a list of paragraphs into the standard list of sections.
+  def or_oar(paragraphs) when is_list(paragraphs) do
+  end
 end
--- test/outliner/outliner_test.exs
+++ test/outliner/outliner_test.exs
@@ -4,4 +4,11 @@ defmodule Outliner.OutlinerTest do
   test "the test file works" do
     assert 1 + 1 == 2
   end
+
+  test "handles the null case" do
+    non_outline = ['<p>Nothing here.</p>']
+    section_form = ['<section class="level-0 non-meta non-outline">Nothing here.</section>']
+
+    assert Outliner.or_oar(non_outline) == section_form
+  end
 end
