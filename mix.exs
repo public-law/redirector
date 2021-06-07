1fe5aebef5e86eb0030669455a6cc36434b72c2d
Robb Shecter <robb@public.law>
2021-06-06 22:45:17 -0600

    getting it working (#11)

--- mix.exs
+++ mix.exs
@@ -32,8 +32,8 @@ defmodule Redirector.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.4"},
-      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix, "~> 1.5.9"},
+      {:phoenix_pubsub, "~> 2.0.0"},
       {:gettext, "~> 0.11"},
       {:plug_cowboy, "~> 1.0"},
       {:plug, "~> 1.7"},
