6f97d2920476d675e2e4a28d680178a9e844ed6b
Robb Shecter <robb@public.law>
2022-08-06 17:45:27 -0600

    refactor

--- lib/redirector_web/controllers/redirect_controller.ex
+++ lib/redirector_web/controllers/redirect_controller.ex
@@ -14,15 +14,19 @@ defmodule RedirectorWeb.RedirectController do
   # General requests
   #
 
-  def bad_request(conn, _params) do
+  def bad_request(conn, _) do
     conn
     |> put_status(400)
     |> text("Error")
   end
 
+  #
+  # Blog
+  #
+
   def blog_feed(conn, _), do: permanent_redirect(conn, to: "https://blog.public.law/feed/")
-  def robb_blog_feed(conn, _), do: permanent_redirect(conn, to: "https://dogsnog.blog/feed/")
   def redirect_rss(conn, _), do: permanent_redirect(conn, to: "https://blog.public.law/rss")
+  def robb_blog_feed(conn, _), do: permanent_redirect(conn, to: "https://dogsnog.blog/feed/")
 
   #
   # oregonlaws.org Redirects
@@ -37,14 +41,13 @@ defmodule RedirectorWeb.RedirectController do
     permanent_redirect(conn, to: "#{@www_url}/dictionary/entries/#{fixed_up_phrase}")
   end
 
-  def redirect_glossary_root(conn, _params),
-    do: permanent_redirect(conn, to: "#{@www_url}/dictionary")
+  def redirect_glossary_root(conn, _), do: permanent_redirect(conn, to: "#{@www_url}/dictionary")
 
   #
   # Other
   #
 
-  def redirect_ors_statutes(conn, _params) do
+  def redirect_ors_statutes(conn, _) do
     permanent_redirect(conn, to: "#{@opl_url}/statutes")
   end
 
@@ -65,42 +68,38 @@ defmodule RedirectorWeb.RedirectController do
   end
 
   def redirect_robots(conn, _),
-    do: permanent_redirect(conn, to: "https://www.public.law/robots.txt")
+    do: permanent_redirect(conn, to: "#{@www_url}/robots.txt")
 
-  def redirect_ads_txt(conn, _),
-    do: permanent_redirect(conn, to: "https://oregon.public.law/ads.txt")
+  def redirect_ads_txt(conn, _), do: permanent_redirect(conn, to: "#{@opl_url}/ads.txt")
 
-  def redirect_sign_in(conn, _),
-    do: permanent_redirect(conn, to: "https://oregon.public.law/users/sign_in")
+  def redirect_sign_in(conn, _), do: permanent_redirect(conn, to: "#{@opl_url}/users/sign_in")
 
   def redirect_sitemap(conn, _),
-    do: permanent_redirect(conn, to: "https://oregon.public.law/sitemaps/sitemap.xml.gz")
+    do: permanent_redirect(conn, to: "#{@opl_url}/sitemaps/sitemap.xml.gz")
 
   def redirect_ors_search(conn, %{"search" => term, "page" => page}) do
     query = URI.encode_query(%{page: page, term: term})
-    permanent_redirect(conn, to: "https://oregon.public.law/search?#{query}")
+    permanent_redirect(conn, to: "#{@opl_url}/search?#{query}")
   end
 
   def redirect_ors_search(conn, %{"search" => term}) do
     query = URI.encode_query(%{term: term})
-    permanent_redirect(conn, to: "https://oregon.public.law/search?#{query}")
+    permanent_redirect(conn, to: "#{@opl_url}/search?#{query}")
   end
 
   #
   # Root path Redrects
   #
 
-  def redirect_root(conn = %{host: "www.oregonlaws.org"}, _params) do
-    permanent_redirect(conn, to: "https://oregon.public.law")
+  def redirect_root(conn = %{host: "www.oregonlaws.org"}, _) do
+    permanent_redirect(conn, to: @opl_url)
   end
 
-  def redirect_root(conn = %{host: "oregonlaws.org"}, _params) do
-    permanent_redirect(conn, to: "https://oregon.public.law")
+  def redirect_root(conn = %{host: "oregonlaws.org"}, _) do
+    permanent_redirect(conn, to: @opl_url)
   end
 
-  def redirect_root(conn, _params) do
-    permanent_redirect(conn, to: "https://www.public.law")
-  end
+  def redirect_root(conn, _), do: permanent_redirect(conn, to: "#{@www_url}")
 
   #
   # Weblaws.org Redirects
@@ -117,7 +116,7 @@ defmodule RedirectorWeb.RedirectController do
     do: do_state_redirect(conn, state, tail)
 
   def redirect_state(conn, %{"segments" => segments}),
-    do: permanent_redirect(conn, to: "https://oregon.public.law/#{Enum.join(segments, "/")}")
+    do: permanent_redirect(conn, to: "#{@opl_url}/#{Enum.join(segments, "/")}")
 
   defp do_state_redirect(conn, state, segments) do
     domain = translate_state(state)
