f668173741acd40cd8a387cc3832da01380d5819
Robb Shecter <robb@public.law>
2022-08-06 16:26:31 -0600

    update

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Redirector.Mixfile do
       version: "0.0.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
