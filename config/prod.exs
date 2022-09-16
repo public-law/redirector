25cef173bddcbb299109a4d5842a5e852c66b671
Robb Shecter <robb@public.law>
2022-09-16 14:24:55 -0600

    remove static manifest config

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,6 @@ import Config
 config :redirector, RedirectorWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
