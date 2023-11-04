206221c78251c8b4541e89e8d6a4b3f1e162053b
Robb Shecter <robb@public.law>
2023-11-03 18:16:11 -0600

    Updates (#24)
    * tmp dir
    
    * new tool versions
    
    * fix: tool versions
    
    * fix: deprecation
    
    * fix: tests

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Redirector.Mixfile do
     [
       app: :redirector,
       version: "0.0.1",
-      elixir: "~> 1.14.0",
+      elixir: "~> 1.15.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
